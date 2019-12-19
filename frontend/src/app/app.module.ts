81256c41abe39b99e03f92d34e9d1d906c532a64
Dalton <dcaron@mtech.edu>
2019-12-18 16:31:31 -0800

    Routing and some kind of site menu bar.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPopUpComponent } from './login-pop-up/login-pop-up.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPopUpComponent
+    LoginPopUpComponent,
+    MainNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
